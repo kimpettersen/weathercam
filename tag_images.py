5b9a0ac9fa6a145266419e459c6f81ddc9578ac4
Kim Pettersen <kim.a.pettersen@gmail.com>
2016-07-28 17:28:33 +0000

    backup

--- tag_images.py
+++ tag_images.py
@@ -1,16 +1,33 @@
 import os
 
-base_path = 'Kendall_Square_datasets/Kendall_Square_'
+import numpy as np
+from PIL import Image
+from sklearn.feature_extraction import image
+
+BASE_PATH = 'Kendall_Square_datasets/Kendall_Square_'
+IMG_SIZE = (210, 137)
+
 
 def create_metadata():
     data = []
     for label in ['sunny', 'cloudy']:
-        file_path = './{}{}/'.format(base_path, label)
+        file_path = './{}{}/'.format(BASE_PATH, label)
         files = os.listdir(file_path)
 
         for img in files:
-            data.append((file_path + img, label))
+            img_path = file_path + img
+            raw_img = read_image(img_path)
+            data.append((raw_img, label))
     return data
+
+
+def read_image(path):
+    img = Image.open(path)
+    img = img.resize(IMG_SIZE)
+    rgb_values = np.array(img.getdata())
+    patches = image.extract_patches_2d(rgb_values, (2,2))
+
+    import ipdb; ipdb.set_trace()
 
 
 if __name__ == '__main__':
